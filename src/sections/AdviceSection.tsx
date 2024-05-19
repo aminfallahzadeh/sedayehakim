02d5a6abf8444a3642b152973b1aad488e53bf72
amin-dev-tech <amin.dev.tech@gmail.com>
2024-05-19 20:54:01 +0300

    add swiper

--- src/sections/AdviceSection.tsx
+++ src/sections/AdviceSection.tsx
@@ -1,3 +1,10 @@
+// swiper js
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination } from "swiper/modules";
+
 function AdviceSection() {
   const content = (
     <section className="advice">
@@ -8,6 +15,38 @@ function AdviceSection() {
               <h3 className="title-primary__heding">توصیه های پزشک</h3>
               <span className="title-primary__accent">صدای حکیم</span>
             </div>
+          </div>
+          <div className="advice__swiper">
+            <Swiper
+              navigation={true}
+              pagination={true}
+              modules={[Navigation, Pagination]}
+            >
+              <SwiperSlide>
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله
+                در ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید
+                سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است
+                چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم
+                است{" "}
+              </SwiperSlide>
+              <SwiperSlide>
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله
+                در ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید
+                سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است
+                چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم
+                است{" "}
+              </SwiperSlide>
+              <SwiperSlide>
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله
+                در ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید
+                سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است
+                چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم
+                است{" "}
+              </SwiperSlide>
+            </Swiper>
           </div>
         </div>
 
