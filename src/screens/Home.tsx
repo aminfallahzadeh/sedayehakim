63c7fcbc15028ce28b5365e4b58e331d6a609c2f
amin-dev-tech <amin.dev.tech@gmail.com>
2024-05-20 14:19:58 +0300

    add responsive

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -11,11 +11,6 @@ import FreeCoursesSection from "../sections/FreeCoursesSection";
 import StatisticsSection from "../sections/StatisticsSection";
 import ColleagueSection from "../sections/ColleagueSection";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import { Navigation } from "swiper/modules";
-
 export default function App(): JSX.Element {
   const content: JSX.Element = (
     <>
@@ -30,17 +25,6 @@ export default function App(): JSX.Element {
       <FreeCoursesSection />
       <StatisticsSection />
       <ColleagueSection />
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
     </>
   );
 
