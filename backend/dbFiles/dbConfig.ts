905fc73c4e04e137fc39191ca5c122aafc7239b9
amin-dev-tech <amin.dev.tech@gmail.com>
2024-05-24 22:52:37 +0300

    configure sql connection

---
+++ backend/dbFiles/dbConfig.ts
@@ -0,0 +1,13 @@
+export const config = {
+  user: "new",
+  password: "123",
+  server: "171.22.24.221",
+  database: "Hakim_DB",
+  options: {
+    trustServerCertificate: true,
+    trustedConnection: true,
+    enableArithAbort: true,
+    instancename: "SRV9600259616",
+  },
+  port: 1422,
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import sql from "mssql";
+import { config } from "../dbFiles/dbConfig";
 
 dotenv.config();
 
@@ -10,8 +12,19 @@ const app = express();
 
 app.use(cors());
 
-app.get("/api", (req: Request, res: Response) => {
-  res.json("API is running...");
+app.get("/api", async (req: Request, res: Response) => {
+  try {
+    await sql.connect(config);
+    const result = await sql.query`SELECT * FROM shop.Product`;
+
+    res.json(result.recordset);
+  } catch (err) {
+    if (err instanceof Error) {
+      res.status(500).send(err.message);
+    } else {
+      res.status(500).send("An unknown error occurred");
+    }
+  }
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
